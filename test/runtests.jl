150d91b223ee10a3a0da3406b268a96b4c2f18f9
Quinten Preiss <qpreiss@smail.uni-koeln.de>
2024-06-27 11:12:44 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,22 +8,22 @@ using Aqua
     end
     @testset "Operations" begin
         @test ZZ() == ZZ()
-        @test nQubits(ZZ()) == 2
-        @test isClifford(ZZ()) == true
-        @test qiskitRepresentation(ZZ()) === nothing
+        @test MonitoredQuantumCircuits.nQubits(ZZ()) == 2
+        @test MonitoredQuantumCircuits.isClifford(ZZ()) == true
+        # @test qiskitRepresentation(ZZ()) === nothing
 
         @test XX() == XX()
-        @test nQubits(XX()) == 2
-        @test isClifford(XX()) == true
-        @test qiskitRepresentation(XX()) === nothing
+        @test MonitoredQuantumCircuits.nQubits(XX()) == 2
+        @test MonitoredQuantumCircuits.isClifford(XX()) == true
+        # @test qiskitRepresentation(XX()) === nothing
 
         @test YY() == YY()
-        @test nQubits(YY()) == 2
-        @test isClifford(YY()) == true
-        @test qiskitRepresentation(YY()) === nothing
+        @test MonitoredQuantumCircuits.nQubits(YY()) == 2
+        @test MonitoredQuantumCircuits.isClifford(YY()) == true
+        # @test qiskitRepresentation(YY()) === nothing
     end
     @testset "Lattice" begin
-        @test getBonds(ChainLattice(3)) = [(1, 2), (2, 3)]
-        @test getBonds(SquareLattice(2, 2)) = [(1, 2), (1, 3), (2, 4), (3, 4)]
+        @test MonitoredQuantumCircuits.getBonds(ChainLattice(3)) == [(1, 2), (2, 3)]
+        @test MonitoredQuantumCircuits.getBonds(SquareLattice(2, 2)) == [(1, 3), (1, 2), (2, 4), (3, 4)]
     end
 end
