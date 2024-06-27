7c96d92a959b47730fb10b0af3116a7f032587e2
Quinten Preiß <preissquinten@gmail.com>
2024-06-27 23:19:23 +0200

    heavyHexagon

--- src/lattices/heavyHexagonLattice.jl
+++ src/lattices/heavyHexagonLattice.jl
@@ -6,17 +6,26 @@ struct HeavyHexagonLattice <: Lattice
     function HeavyHexagonLattice(sizeX::Integer, sizeY::Integer)
         sizeX > 0 || throw(ArgumentError("size must be positive"))
         sizeY > 0 || throw(ArgumentError("size must be positive"))
-        graph = grid([sizeX + 1, sizeY]; periodic=false)
+        graph = grid([sizeX, sizeY]; periodic=false)
 
         for j in 1:sizeY-1
-            for i in (j%2+1+(j-1)*(sizeX+1)):2:j*(sizeX+1)
-                rem_edge!(graph, i, i + sizeX + 1)
+            for i in (j%2+1+(j-1)*(sizeX)):2:j*(sizeX)
+                rem_edge!(graph, i, i + sizeX)
             end
         end
-        rem_edge!(graph, sizeX, sizeX + 1)
-        rem_edge!(graph, (sizeY - 1) * (sizeX + 1), (sizeY - 1) * (sizeX + 1) + 1)
-        rem_vertex!(graph, sizeX + 1)
-        rem_vertex!(graph, (sizeY - 1) * (sizeX + 1) + 1)
+        # rem_edge!(graph, sizeX, sizeX + 1)
+        # rem_edge!(graph, (sizeY - 1) * (sizeX + 1), (sizeY - 1) * (sizeX + 1) + 1)
+        # if isodd(sizeY) && isodd(sizeX)
+        #     rem_vertex!(graph, sizeX + 1)
+        #     rem_vertex!(graph, (sizeY - 1) * (sizeX + 1) + 1)
+        # elseif isodd(sizeY) && iseven(sizeX)
+        #     rem_vertex!(graph, nv(graph))
+        #     rem_vertex!(graph, (sizeY - 1) * (sizeX + 1) + 1)
+        # elseif iseven(sizeY) && isodd(sizeX)
+        #     rem_vertex!(graph, nv(graph))
+        #     rem_vertex!(graph, sizeX + 1)
+        # end
+
         nNodes = nv(graph)
         for e in collect(edges(graph))
             src = Graphs.src(e)
@@ -33,81 +42,56 @@ struct HeavyHexagonLattice <: Lattice
     end
 end
 function visualize(io::IO, lattice::HeavyHexagonLattice)
-    for j in 1:lattice.sizeY-1
-        if j % 2 == 1
-            for _ in 1:2lattice.sizeX-2
-                print(io, "o - ")
-            end
-            println(io, "o")
-            for i in 1:2lattice.sizeX-2
-                if i % 2 == 1
-                    print(io, "|   ")
-                else
-                    print(io, "    ")
+
+    for _ in 1:2lattice.sizeX-2
+        print(io, "o - ")
+    end
+    println(io, "o")
+    for j in 2:2lattice.sizeY-1
+        if j % 2 == 0
+            if j % 4 == 0
+                print(io, "        ")
+                for i in 1:2:lattice.sizeX-3
+                    print(io, "|               ")
                 end
-            end
-            println(io, "|")
-            for i in 1:2lattice.sizeX-2
-                if i % 2 == 1
-                    print(io, "o   ")
-                else
-                    print(io, "    ")
+                println(io, "|")
+                print(io, "        ")
+                for i in 1:2:lattice.sizeX-3
+                    print(io, "o               ")
                 end
-            end
-            println(io, "o")
-            for i in 1:2lattice.sizeX-2
-                if i % 2 == 1
-                    print(io, "|   ")
-                else
-                    print(io, "    ")
+                println(io, "o")
+                print(io, "        ")
+                for i in 1:2:lattice.sizeX-3
+                    print(io, "|               ")
                 end
+                println(io, "|")
+
+            else
+                for i in 1:2:lattice.sizeX-2
+                    print(io, "|               ")
+                end
+                println(io, "|")
+                for i in 1:2:lattice.sizeX-2
+                    print(io, "o               ")
+                end
+                println(io, "o")
+                for i in 1:2:lattice.sizeX-2
+                    print(io, "|               ")
+                end
+                println(io, "|")
             end
-            println(io, "|")
-            print(io, "o - o - ")
 
         else
             for _ in 1:2lattice.sizeX-2
                 print(io, "o - ")
             end
             println(io, "o")
-            print(io, "        |   ")
-            for i in 2:2lattice.sizeX-2
-                if i % 2 == 1
-                    print(io, "|   ")
-                else
-                    print(io, "    ")
-                end
-            end
-            println(io, "|")
-            print(io, "        o   ")
-            for i in 2:2lattice.sizeX-2
-                if i % 2 == 1
-                    print(io, "o   ")
-                else
-                    print(io, "    ")
-                end
-            end
-            println(io, "o")
-            print(io, "        |   ")
-            for i in 2:2lattice.sizeX-2
-                if i % 2 == 1
-                    print(io, "|   ")
-                else
-                    print(io, "    ")
-                end
-            end
-            println(io, "|")
         end
     end
-    if lattice.sizeY % 2 == 0
-        print(io, "o - ")
-    else
-        print(io, "        ")
-    end
-    for _ in 1:2lattice.sizeX-2
-        print(io, "o - ")
-    end
-    println(io, "o")
-
+    # print(io, "        ")
+    # for _ in 1:2lattice.sizeX-2
+    #     print(io, "o - ")
+    # end
+    # println(io, "o")
     return nothing
 end
