3cd1d8465bc3e70472f0d48e0ee2d661d7fec4b6
Documenter.jl <documenter@juliadocs.github.io>
2024-08-28 08:20:20 +0000

    build based on 1bbacb0

--- dev/assets/api.md.CowoUNdx.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as i,c as t,o as s,a7 as e}from"./chunks/framework.Ck1NKG2X.js";const b=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),a={name:"api.md"},r=e("",20),n=[r];function o(p,d,l,h,u,c){return s(),t("div",null,n)}const g=i(a,[["render",o]]);export{b as __pageData,g as default};
