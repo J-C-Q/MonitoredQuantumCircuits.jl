e1444a94e3ed6e61515a841a991c0e7af0a2f6fd
Quinten Preiß <preissquinten@gmail.com>
2024-08-28 22:32:04 +0200

    example

--- examples/phaseDiagramData.jl
+++ examples/phaseDiagramData.jl
@@ -1,23 +1,20 @@
 using Distributed;
-addprocs();
-using Dagger
+using SharedArrays;
+addprocs(6);
+# List of all worker IDs
+worker_ids = workers()
 using MonitoredQuantumCircuits
+# Load the package on each worker one by one
+for wid in worker_ids
+    @eval @everywhere if myid() == $wid
+        using MonitoredQuantumCircuits
+    end
+    sleep(1)
+end
+
 using ProgressMeter
 using JLD2
 
-
-
-function doWork!(tmis, i, px, py, pz, shots, lattice, backend)
-    circuit = KitaevCircuit(lattice, px, py, pz, 24^2)
-
-    result = execute(circuit, backend; shots, verbose=false)
-
-    bits = result[end-MonitoredQuantumCircuits.nQubits(lattice)+1:end]
-
-    tripartiteInformation = Analysis.TMI(bits, [1, 2, 7, 8, 13, 14, 19, 20], [3, 4, 9, 10, 15, 16, 21, 22], [5, 6, 11, 12, 17, 18, 23, 24])
-
-    tmis[i] = tripartiteInformation
-end
 
 
 
@@ -35,9 +32,18 @@ function generateData(; grain=0.1, shots=10000)
         end
     end
 
-    tmis = Vector{Float64}(undef, length(points))
-    @showprogress for (i, (px, py, pz)) in enumerate(points)
-        doWork!(tmis, i, px, py, pz, shots, lattice, backend)
+    tmis = SharedVector{Float64}(length(points))
+    @showprogress @distributed for i in 1:length(points)
+        px, py, pz = points[i]
+        circuit = KitaevCircuit(lattice, px, py, pz, 24^2)
+
+        result = execute(circuit, backend; shots, verbose=false)
+
+        bits = result[end-MonitoredQuantumCircuits.nQubits(lattice)+1:end]
+
+        tripartiteInformation = Analysis.TMI(bits, [1, 2, 7, 8, 13, 14, 19, 20], [3, 4, 9, 10, 15, 16, 21, 22], [5, 6, 11, 12, 17, 18, 23, 24])
+
+        tmis[i] = tripartiteInformation
     end
 
     # wait(collect(Dagger.@spawn))
