763acd855980482c1f6e7de3239116d8609d75c0
Quinten Preiss <qpreiss@smail.uni-koeln.de>
2024-08-30 17:41:27 +0200

    periodic

--- src/circuits/kitaev.jl
+++ src/circuits/kitaev.jl
@@ -13,15 +13,19 @@ end
 
 function KitaevCircuit(lattice::HexagonToricCodeLattice)
     lattice.sizeX % 2 == 0 || throw(ArgumentError("The sizeX must be even"))
-    cycles = mysimplecycles_limited_length(lattice.graph, 12, 10^6)
-    @info cycles
-    test = [(cycle[i], cycle[mod1(i + 1, length(cycle))], cycle[mod1(i + 2, length(cycle))], mod1(i, 3), mod1(i, 3)) for cycle in cycles for i in 1:2:length(cycle)]
-    test = unique!(x -> Set(x[1:3]), test)
-    operations = Operation[ZZ(), XX(), YY()]
-    operationPositions = [(i, j, k) for (i, j, k, _, _) in test]
-    operationPointers = [ptr for (_, _, _, ptr, _) in test]
-    executionOrder = [ord for (_, _, _, _, ord) in test]
-    return Circuit(lattice, operations, operationPositions, operationPointers, executionOrder)
+    lattice.sizeY % 2 == 0 || throw(ArgumentError("The sizeY must be even"))
+    [(neighbors(lattice.graph, i)..., i) for i in findfirst(x -> x == 1, lattice.isAncilla):nv(lattice.graph)]
+    # cycles = mysimplecycles_limited_length(lattice.graph, 12, 10^6)
+
+
+    # @info cycles
+    # test = [(cycle[i], cycle[mod1(i + 1, length(cycle))], cycle[mod1(i + 2, length(cycle))], mod1(i, 3), mod1(i, 3)) for cycle in cycles for i in 1:2:length(cycle)]
+    # test = unique!(x -> Set(x[1:3]), test)
+    # operations = Operation[ZZ(), XX(), YY()]
+    # operationPositions = [(i, j, k) for (i, j, k, _, _) in test]
+    # operationPointers = [ptr for (_, _, _, ptr, _) in test]
+    # executionOrder = [ord for (_, _, _, _, ord) in test]
+    # return Circuit(lattice, operations, operationPositions, operationPointers, executionOrder)
 end
 
 function KitaevCircuit(lattice::HeavyHexagonLattice, layers::Integer)
