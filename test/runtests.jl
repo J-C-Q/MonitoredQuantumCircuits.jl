015e6945997ad7473d475d36240d1ad11d9f9772
Quinten Preiss <qpreiss@smail.uni-koeln.de>
2025-03-31 11:20:08 +0200

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,20 +7,20 @@ using Aqua
     #     Aqua.test_all(MonitoredQuantumCircuits)
     # end
     @testset "Operations" begin
-        @test ZZ() == ZZ()
-        @test MonitoredQuantumCircuits.nQubits(ZZ()) == 3
-        @test MonitoredQuantumCircuits.isClifford(ZZ()) == true
-        # @test qiskitRepresentation(ZZ()) === nothing
+        # @test ZZ() == ZZ()
+        # @test MonitoredQuantumCircuits.nQubits(ZZ()) == 3
+        # @test MonitoredQuantumCircuits.isClifford(ZZ()) == true
+        # # @test qiskitRepresentation(ZZ()) === nothing
 
-        @test XX() == XX()
-        @test MonitoredQuantumCircuits.nQubits(XX()) == 3
-        @test MonitoredQuantumCircuits.isClifford(XX()) == true
-        # @test qiskitRepresentation(XX()) === nothing
+        # @test XX() == XX()
+        # @test MonitoredQuantumCircuits.nQubits(XX()) == 3
+        # @test MonitoredQuantumCircuits.isClifford(XX()) == true
+        # # @test qiskitRepresentation(XX()) === nothing
 
-        @test YY() == YY()
-        @test MonitoredQuantumCircuits.nQubits(YY()) == 3
-        @test MonitoredQuantumCircuits.isClifford(YY()) == true
-        # @test qiskitRepresentation(YY()) === nothing
+        # @test YY() == YY()
+        # @test MonitoredQuantumCircuits.nQubits(YY()) == 3
+        # @test MonitoredQuantumCircuits.isClifford(YY()) == true
+        # # @test qiskitRepresentation(YY()) === nothing
     end
     @testset "Lattice" begin
         # @test MonitoredQuantumCircuits.getBonds(ChainLattice(3)) == [(1, 2), (2, 3)]
