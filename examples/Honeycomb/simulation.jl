475d941ae210b31453f303360d7f15290d55426d
Quinten Preiss <qpreiss@smail.uni-koeln.de>
2025-07-21 10:20:04 +0200

    example threading

--- examples/Honeycomb/simulation.jl
+++ examples/Honeycomb/simulation.jl
@@ -1,25 +1,33 @@
-function simulate_QuantumClifford(L,n=10;shots=10,depth=10)
+function simulate_QuantumClifford(L,n=10;shots=10,depth=10,type=:Kitaev)
     g = HoneycombGeometry(Periodic, L, L)
-    ps = generateProbs(;n)
-    backend = QuantumClifford.TableauSimulator(nQubits(g); mixed=true)
+    ps = generateProbs(;n, offset=0.01)
+
 
     partitionsX = subsystems(g, 4; cutType=:X)
     partitionsY = subsystems(g, 4; cutType=:Y)
     partitionsZ = subsystems(g, 4; cutType=:Z)
 
     tmi = zeros(Float64, length(ps))
-    for (i, (px,py,pz)) in enumerate(ps)
+    Threads.@threads for i in eachindex(ps)
+        px,py,pz = ps[i]
+        backend = QuantumClifford.TableauSimulator(nQubits(g); mixed=true)
         post = (s) -> begin
             tmi[i] += QuantumClifford.tmi(backend.state, partitionsX)
             tmi[i] += QuantumClifford.tmi(backend.state, partitionsY)
             tmi[i] += QuantumClifford.tmi(backend.state, partitionsZ)
         end
-        execute!(()->measurementOnlyKitaev!(backend, g, px,py,pz; depth), backend, post; shots=shots)
+        if type == :Kitaev
+            execute!(()->measurementOnlyKitaev!(backend, g, px,py,pz; depth), backend, post; shots=shots)
+        elseif type == :Kekule
+            execute!(()->measurementOnlyKekule!(backend, g, px,py,pz; depth), backend, post; shots=shots)
+        else
+            throw(ArgumentError("Unsupported type $type. Choose one of :Kitaev, :Kekule"))
+        end
     end
     return tmi ./= 3shots
 end
 
-function generateProbs(; n=45)
+function generateProbs(; n=45,offset=0.0)
     points = Vector{NTuple{3,Float64}}(undef, n*(n + 1) ÷ 2)
     m = 1
     for (k, i) in enumerate(range(0, 1, n))
@@ -32,5 +40,5 @@ function generateProbs(; n=45)
         end
     end
 
-    return points
+    return [p .- offset .* (p .- (1 / 3, 1 / 3, 1 / 3)) for p in points]
 end
--- examples/TransverseFieldIsingFibonacci/simulation.jl
+++ examples/TransverseFieldIsingFibonacci/simulation.jl
@@ -1,9 +1,12 @@
+using MonitoredQuantumCircuits
 function simulate_QuantumClifford(L,ps;shots=10,depth=10)
     g = ChainGeometry(Periodic, L)
-    backend = QuantumClifford.TableauSimulator(nQubits(g); mixed=false, basis=:X)
+
 
     entanglement = zeros(Float64, length(ps))
-    for (i, p) in enumerate(ps)
+    Threads.@threads for i in eachindex(ps)
+        backend = QuantumClifford.TableauSimulator(nQubits(g); mixed=false, basis=:X)
+        p = ps[i]
         post = (s) -> begin
             entanglement[i] += QuantumClifford.entanglement_entropy(backend.state, 1:div(L,2))
         end
